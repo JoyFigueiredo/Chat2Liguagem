4f223c61064479c608b856cd2a90aea75e869fb4
Joice Figueiredo <joicebf1995@outlook.com>
2022-11-08 10:26:26 -0300

    Main

--- src/socket/Servidor.java
+++ src/socket/Servidor.java
@@ -11,16 +11,10 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
 import java.io.Writer;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
-import java.util.List;
-
-import javax.swing.JLabel;
-import javax.swing.JOptionPane;
-import javax.swing.JTextField;
 
 public class Servidor extends Thread {
 
